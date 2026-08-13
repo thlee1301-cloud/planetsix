4c86de7ab2818606dff72d9574ada9fb58294f56
thlee1301-cloud <thlee1301@gmail.com>
2026-08-13 23:58:19 +0900

    Update works-data.js

--- works-data.js
+++ works-data.js
@@ -96,7 +96,8 @@ year: "2026",
 description: "100여 년 전 한국에 전해진 의료선교의 정신에서 시작해 오늘날 캄보디아로 이어지는 사랑과 헌신의 여정을 생성형 AI와 시네마틱 영상 언어로 재구성한 작품입니다.",
 thumbnail: "assets/works/love-comes-first.jpg?v=20260811-1",
 youtubeId: "cAZ0ALQtua4"
-},
+}
+,
 {
   title: "PLANET SIX HIGHLIGHT",
   englishTitle: "PLANET SIX HIGHLIGHT",
